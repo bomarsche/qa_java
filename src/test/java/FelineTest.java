5a970b4eb9b2a7cad8ede14c86d980d8f29b59ca
Sergey Korobov <bomarsche@gmail.com>
2023-04-21 08:49:14 +0300

    Sprint_6 project

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,26 @@
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
+
+
+public class FelineTest {
+    @Test
+    public void checkFelineEatMeat() throws Exception {
+        Feline feline = new Feline();
+        List<String> result = feline.eatMeat();
+        Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), result);
+    }
+
+    @Test
+    public void checkFelineGetFamily() {
+        Feline feline = new Feline();
+        Assert.assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void checkFelineGetKittens() {
+        Feline feline = new Feline();
+        Assert.assertEquals(1, feline.getKittens());
+    }
+}
